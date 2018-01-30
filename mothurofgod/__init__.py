1441c7359bf2b5cde52aa72b9b7bda01e3befc82
Marc Michel <mmichel1@samsung.local>
2018-01-30 20:04:34 +0100

    Initial structure

---
+++ mothurofgod/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Description
+
+@author: Poireau Lunaire
+@email: 
+@project: https://github.com/A-BN/mothur_of_god
+"""
+
+
+def MothurOfGod():
+    pass
+
+
+if __name__ == '__main__':
+    MothurOfGod()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="mothurofgod",
+    version="0.1",
+    description="The funniest joke in the world",
+    url="https://github.com/A-BN/mothur_of_god",
+    author="Poireau Lunaire",
+    author_email="",
+    license="GPL3",
+    packages=["mothurofgod"],
+    zip_safe=False)
